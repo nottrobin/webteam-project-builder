5699c32310b6aad8b6bcccf7b1ccda94de67d1b1
Robin Winslow <robin@robinwinslow.co.uk>
2015-11-25 15:36:46 +0000

    Improvements; example jenkins script

--- jenkins-build.example.sh
+++ jenkins-build.example.sh
@@ -1,19 +1,9 @@
-# build-scripts
-
-A set of scripts for managing builds of webteam projects.
-
-## Usage
-
-You may use any of the scripts to like, but here's a pretty common pattern
-for use:
-
-``` bash
-project_name="my-project"
+project_name="example-project"
 
 # Prepare the code folder
 ./fetch-project.sh ${project_name}      # Pull down the latest project code
-./node-dependencies.sh ${project_name}  # Create node_modules
 ./compile-sass.sh ${project_name}       # Create .css files from .scss files
+./node-dependencies.sh ${project_name}  # Create node_modules
 
 # Create .tar.gz build archive & save the git version ID
 version=$(./create-build.sh ${project_name})
@@ -22,4 +12,3 @@ version=$(./create-build.sh ${project_name})
 source ~/.novarc  # Setup the swift login credentials
 ./upload-build.sh ${project_name} ${version}       # Upload the built version to swift
 ./set-staging-build.sh ${project_name} ${version}  # Flag this version for staging
-```
